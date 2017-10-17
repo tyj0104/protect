5b0ef26281cb9ceb185c2f7c46c0976f1552a0de
hulele <hulele@znds.com>
2017-10-17 17:52:16 +0800

    feature:登录页面

--- app/src/main/java/com/example/jay/myapplication/ui/main/MainActivity.java
+++ app/src/main/java/com/example/jay/myapplication/ui/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.jay.myapplication.ui;
+package com.example.jay.myapplication.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ app/src/main/java/com/example/jay/myapplication/ui/register/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.jay.myapplication.ui.register;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/10/17
+ */
+
+public class RegisterActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
