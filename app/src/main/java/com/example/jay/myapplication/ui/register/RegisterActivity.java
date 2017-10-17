ef5c5e8b41303733a6d2d819dcc3fec08a9d314e
hulele <hulele@znds.com>
2017-10-17 18:07:22 +0800

    加入了注册的UI

--- app/src/main/java/com/example/jay/myapplication/ui/register/RegisterActivity.java
+++ app/src/main/java/com/example/jay/myapplication/ui/register/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.jay.myapplication.ui.register;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.jay.myapplication.R;
+
 /**
  * Created by Administrator on 2017/10/17
  */
@@ -11,5 +13,6 @@ public class RegisterActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
     }
 }
