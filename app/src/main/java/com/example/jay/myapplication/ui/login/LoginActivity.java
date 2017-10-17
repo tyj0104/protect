8d4ac8c40ea277b69507afd468c7db37d0a5b319
hulele <hulele@znds.com>
2017-10-17 16:21:55 +0800

    建立develop分支，本地代码都合到此分支上

---
+++ app/src/main/java/com/example/jay/myapplication/ui/login/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.jay.myapplication.ui.login;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2017/10/17
+ */
+
+public class LoginActivity extends Activity {
+}
